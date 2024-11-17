7dfe498dee084c2c45e183c1273a05dc4b5c42c8
ObicLaravel3 <158170521+ObicLaravel3@users.noreply.github.com>
2024-11-17 15:48:43 +0300

    space

--- src/features/chat/Components/Partials/ChatTab.tsx
+++ src/features/chat/Components/Partials/ChatTab.tsx
@@ -2,9 +2,9 @@ import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { FaHome, FaDollarSign, FaClipboardList, FaBook, FaBlog } from "react-icons/fa"; // استيراد الأيقونات
 
-export const ChatTab = () => {
+export const ChatTab = () => {  
   return (
-    <div className="bg-neutral-100 pt-0 flex">
+    <div className="bg-neutral-100  flex">
       <SlideTabs />
     </div>
   );
@@ -25,7 +25,7 @@ const SlideTabs = () => {
           opacity: 0,
         }));
       }}
-      className="relative flex flex-col items-start left-0 fixed h-full p-4 space-y-4"
+      className="relative flex  items-start  fixed h-full space-y-4"
     >
       <Tab setPosition={setPosition} icon={<FaHome />} />
       <Tab setPosition={setPosition} icon={<FaDollarSign />} />
@@ -37,7 +37,6 @@ const SlideTabs = () => {
     </ul>
   );
 };
-
 const Tab = ({ icon, setPosition }) => {
   const ref = useRef(null);
 
@@ -58,12 +57,13 @@ const Tab = ({ icon, setPosition }) => {
       className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference"
     >
       <div className="relative flex flex-col items-center">
-        <div className="text-2xl md:text-3xl">{icon}</div>
+        <div className="text-xl md:text-2xl">{icon}</div> {/* تعديل الحجم هنا */}
         <div className="absolute bottom-0 w-full h-0.5 bg-white opacity-0 transition-all duration-300"></div>
       </div>
     </li>
   );
 };
+
 
 const Cursor = ({ position }) => {
   return (
--- src/features/chat/Layout/ChatLayout.tsx
+++ src/features/chat/Layout/ChatLayout.tsx
@@ -5,7 +5,7 @@ import Navbar from "../Components/Partials/Navbar";
 
 function ChatLayout() {
   return (
-    <div className="flex flex-col min-h-screen p-4 top-0   ">
+    <div className="flex flex-col min-h-screen  top-0 py-4  ">
 
 
       {/* ===== Header Start ===== */}
