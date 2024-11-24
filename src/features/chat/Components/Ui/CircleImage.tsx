b2d5829bdac600366195d6dbebf1277c3b8a8101
ObicLaravel1 <159122054+ObicLaravel1@users.noreply.github.com>
2024-11-24 09:00:04 +0300

    Add files via upload

---
+++ src/features/chat/Components/Ui/CircleImage.tsx
@@ -0,0 +1,27 @@
+
+
+import React from "react";
+
+interface CircleImageProps {
+  size: number; 
+  imageUrl: string; 
+  border?: string; 
+}
+
+const CircleImage: React.FC<CircleImageProps> = ({ size, imageUrl, border }) => {
+ 
+
+  return (
+    <img
+     src={imageUrl}
+     alt='image'
+     className={`w-${size} h-${size} rounded-full object-cover ${border || ""}`}
+  />
+    
+  );
+};
+
+export default CircleImage;
+
+
+
