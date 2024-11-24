74c1a93ef9dcf2bb27ba3e7758d1a53e2b9df9e5
safiawebdesigner <118714614+safiawebdesigner@users.noreply.github.com>
2024-11-24 22:28:42 +0300

    24-11

---
+++ src/features/chat/pages/protected/status/statusData.tsx
@@ -0,0 +1,26 @@
+ export const statusData = [
+    {
+      id: 1,
+      username: "samar",
+      time: "Today at 12:30 pm",
+      imageUrls: [
+        "https://images.pexels.com/photos/28996347/pexels-photo-28996347/free-photo-of-vibrant-dahlia-flowers-in-glass-vases.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://via.placeholder.com/60",
+        "https://via.placeholder.com/60",
+        "https://via.placeholder.com/60",
+        "https://via.placeholder.com/60",
+        "https://via.placeholder.com/60",
+        "https://images.pexels.com/photos/28853291/pexels-photo-28853291/free-photo-of-autumn-train-journey-through-swedish-countryside.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/29401471/pexels-photo-29401471/free-photo-of-cozy-vintage-cafe-corner-with-artistic-flair.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      ], 
+  
+    },
+    {
+      id: 2,
+      username: "said",
+      time: "Today at 9:30 pm",
+      imageUrls: ["https://images.pexels.com/photos/28467153/pexels-photo-28467153/free-photo-of-charming-colonial-facade-in-paraty-brazil.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"], // Single image for this status
+    
+    },
+  ];
+  
