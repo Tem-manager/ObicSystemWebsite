c1736f8b88b8e2747a942c073e99bb7d2a30a607
safiawebdesigner <118714614+safiawebdesigner@users.noreply.github.com>
2024-11-25 09:04:12 +0300

    25-11

--- src/features/chat/pages/protected/chats/all-chats/AllChats.tsx
+++ src/features/chat/pages/protected/chats/all-chats/AllChats.tsx
@@ -10,41 +10,30 @@ function AllChats() {
         <ChatTab />
       <div className="flex-grow ml-[10px]  w-full  flex  ">
       <ChatList/>
-<<<<<<< Updated upstream
       <div className="w-[65%] bg-gray-200 p-4 m-2 ">
       <Header
-      // الجهة اليسرى: صورة دائرية مع نص بجانبها
-      leftContent={
-        <>
+            // الجهة اليسرى: صورة دائرية مع نص بجانبها
+            leftContent={<>
               <div className="flex items-center space-x-3">
-  <CircleImage 
-    imageUrl="https://images.pexels.com/photos/28996347/pexels-photo-28996347/free-photo-of-vibrant-dahlia-flowers-in-glass-vases.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-    size={3} // Adjust size as needed
-  />
-  <span className="text-lg font-semibold text-gray-800">Username</span>
-</div>
+                <CircleImage
+                  imageUrl="https://images.pexels.com/photos/28996347/pexels-photo-28996347/free-photo-of-vibrant-dahlia-flowers-in-glass-vases.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                  size={3} // Adjust size as needed
+                />
+                <span className="text-lg font-semibold text-gray-800">Username</span>
+              </div>
 
-        </>
-
-      }
-      // الجهة اليمنى: أيقونات الفيديو والاتصال
-      rightContent={
-        <div className="flex items-center space-x-4 text-gray-800">
-     <button type="button" className="p-2">
-      <Videocam  />
-    </button>
-          <button type="button">
-            <Call />
-          </button>
-        </div>
-      }
-      className="bg-white shadow-md dark:bg-gray-800 fixed   z-50"
-
+            </>}
+            // الجهة اليمنى: أيقونات الفيديو والاتصال
+            rightContent={<div className="flex items-center space-x-4 text-gray-800">
+              <button type="button" className="p-2">
+                <Videocam />
+              </button>
+              <button type="button">
+                <Call />
+              </button>
+            </div>}
+            className="bg-white shadow-md dark:bg-gray-800 fixed   z-50" centerContent={undefined}
     />
-=======
-      <div className="w-[70%] bg-gray-200 p-4 m-2">
- 
->>>>>>> Stashed changes
       </div>
       </div>
       </>
