7961a08f8704ec3d770d1839dc741a8ae0c58efc
ObicLaravel3 <158170521+ObicLaravel3@users.noreply.github.com>
2024-12-09 16:06:09 +0300

    include: ['swiper']

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['swiper'], // تضمين مكتبة Swiper في التحسينات
+  },
+   server: {
+    host: '0.0.0.0', // هذا يتيح الوصول إلى التطبيق عبر الشبكة المحلية
+    port: 3000,       // يمكنك تغيير المنفذ إذا لزم الأمر
+    open: true,       // يفتح المتصفح تلقائيًا عند بدء التشغيل
+  },
 })
